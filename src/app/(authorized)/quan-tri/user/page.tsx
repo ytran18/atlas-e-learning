28dc3f79146b8826b84422e02126b85c7b995f3e
Trần Như Ý <nhuy@evol.vn>
2025-11-01 14:02:24 +0700

    feat: add loading state to user management page with Suspense

--- src/app/(authorized)/quan-tri/user/page.tsx
+++ src/app/(authorized)/quan-tri/user/page.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 import AdminUserPage from "@/features/quan-tri/pages/AdminUserPage";
 
 const UserPage = () => {
-    return <AdminUserPage />;
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <AdminUserPage />;
+        </Suspense>
+    );
 };
 
 export default UserPage;
